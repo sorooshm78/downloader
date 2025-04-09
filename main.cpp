991566b5fc556faf08bf7bc48c0c1ef264e5b61c
soroush.m <sorooshm78@gmail.com>
2025-04-09 06:40:11 +0000

    split file

--- main.cpp
+++ main.cpp
@@ -2,10 +2,11 @@
 #include <string>
 #include <curl/curl.h>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
-static size_t WriteCallback(void* contents, size_t size, size_t mem_byte, void* user_data) {
+size_t WriteCallback(void* contents, size_t file_size, size_t mem_byte, void* user_data) {
     ofstream* file = static_cast<ofstream*>(user_data);
     
     if (file) {
@@ -15,13 +16,13 @@ static size_t WriteCallback(void* contents, size_t size, size_t mem_byte, void* 
     return 0;
 }
 
-bool DownloadFile(const string& url, const string& outputFilename) {
+bool DownloadFile(const string& url, const string& output_filename) {
     CURL* curl;
     CURLcode res;
-    ofstream outFile(outputFilename, ios::binary);
+    ofstream outFile(output_filename, ios::binary);
     
     if (!outFile) {
-        cerr << "Failed to open output file: " << outputFilename << endl;
+        cerr << "Failed to open output file: " << output_filename << endl;
         return false;
     }
 
@@ -52,15 +53,67 @@ bool DownloadFile(const string& url, const string& outputFilename) {
     return true;
 }
 
+long GetFileSize(const string& url) {
+    CURL *curl = curl_easy_init();  
+
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+    curl_easy_setopt(curl, CURLOPT_NOBODY, 1L);
+    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+    
+    CURLcode res = curl_easy_perform(curl);
+    if (res != CURLE_OK){
+        cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << endl;
+        curl_easy_cleanup(curl);
+        return 0;
+    }
+
+    double file_size = 0;
+    res = curl_easy_getinfo(curl, CURLINFO_CONTENT_LENGTH_DOWNLOAD, &file_size);
+    curl_easy_cleanup(curl);
+    
+    if (res != CURLE_OK || file_size < 0) {
+        cerr << "Failed to get file file_size" << endl;
+        return 0;
+    }
+    return static_cast<long>(file_size);
+}
+
+struct DownloadPart {
+    string url;
+    string filename;
+    long start_byte;
+    long end_byte;
+    bool success;
+};
+
 int main() {
     string url = "https://file-examples.com/storage/fe2465184067ef97996fb41/2017/10/file-sample_150kB.pdf";
-    string outputFilename = "sample.pdf";
+    string output_filename = "sample.pdf";
 
-    if (DownloadFile(url, outputFilename)) {
-        cout << "File downloaded successfully to: " << outputFilename << endl;
-    } else {
-        cout << "Failed to download file." << endl;
+    long file_size = GetFileSize(url);
+    cout << "file_size : " << file_size << endl;
+
+    const int num_parts = 5;
+    int part_size = file_size / num_parts;
+    vector<DownloadPart> download_part;
+
+    for (int i = 0; i < num_parts; ++i) {
+        long start = i * part_size;
+        long end = (i == num_parts - 1) ? file_size - 1 : start + part_size - 1;
+        string part_filename = output_filename + ".part" + to_string(i);
+        download_part.push_back({url, part_filename, start, end, false});
     }
+
+    // for (auto dp : download_part){
+    //     cout << "part filename" << dp.filename << endl; 
+    //     cout << "strat-end : " << dp.start_byte << "-" << dp.end_byte << endl; 
+    // }
+
+    // if (DownloadFile(url, output_filename)) {
+    //     cout << "File downloaded successfully to: " << output_filename << endl;
+    // } else {
+    //     cout << "Failed to download file." << endl;
+    // }
     
     return 0;
 }
